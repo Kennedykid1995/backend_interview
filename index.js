9d7b53c0faea032519b56d67e1980efbcf79fb7c
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-01-15 20:23:59 -0600

    commented in some information for myself to study

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const http = require('http');
+const http = require('http'); //built in node.js module to handle http traffic
 
-const hostname = '127.0.0.1';
-const port = 3000; 
+const hostname = '127.0.0.1'; //local computer where the server is running
+const port = 3000; // a port we'll use to watch for traffic
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World from Node\n');
+    //creates the server
+    res.statusCode = 200; //http status code returned to the client
+    res.setHeader('Content-Type', 'text/plain'); //inform the client that we'll be returing text
+    res.end('Hello World from Node\n');// end the request and send a response with the specific message
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server runing at http://${hostname}:${port}/`);
+    // start watching for connectiong on the port specified
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
 
