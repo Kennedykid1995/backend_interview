9cf3dd7283e489347f3c45595bb757f9cdb9825a
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-01-22 10:43:20 -0600

    working on the server for backend interview

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');  //import the express package
 
 const server = express(); // creates the server
-
+server.use(express.json());
 //handle requests to the root of the api, the / route
 server.get('/', (req, res) => {
     res.send('Hello from Express');
@@ -20,6 +20,14 @@ server.get('/names', (req, res) => {
     ];
     res.status(200).json(names); 
 })
+let nextId = 3; 
+
+server.post('/names', (req, res) => {
+    const name = req.body;
+    name.id = nextId;
+    res.status(201).json(names); 
+});
+
 
 server.listen(5000, () => 
     console.log('Server running on http://localhost:5000')
