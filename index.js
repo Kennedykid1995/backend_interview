08a4cc8f56d17e1efe88af382ba47b9b6c3f9485
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-01-28 11:35:25 -0600

    working on starting up a server

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+//Backend MVP
+// Add data persistence using a Relational Database. We suggest you start with SQLite3.
+// Create a Web API for the React application you built in the front-end project week.
+// Build endpoints for each of the following features:
+//   Display a list of notes.
+//   Create a note with a title and content.
+//   View an existing note.
+//   Edit an existing note.
+//   Delete an existing note.
+//   Modify your front-end so that it uses your newly created Web API.
+//Deploy through Heroku
 
+require('dotenv').config(); 
+
+const server = require('./api/server.js');
+
+const port = 5000; 
+server.listen(port, () => {
+    console.log(`**** Server up on PORT ${port} ****`)
+})
