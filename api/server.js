89b098cde3e775d6e87b6a75c74a30c03068902b
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-01-28 13:05:09 -0600

    setting up the end points

--- api/server.js
+++ api/server.js
@@ -6,5 +6,26 @@ server.use(express.json(), cors());
 
 server.get('/', (req, res) => {
     res.send("Hello from Express")
-})
+});
+
+server.get('/notes', (req, res) => {
+    //all notes 
+});
+
+server.get('/notes/:id', (req, res) => {
+    //a note
+});
+
+server.post('/notes', (req, res) => {
+    //post to notes 
+});
+
+server.put('/notes/:id', (req, res) => {
+    //edit a note 
+});
+
+server.delete('/notes/:id', (req, res) => {
+    //delete a note 
+});
+
 module.exports = server; 
