cf919cc33fd1c0f6a8ad09fe77a4631991f787a8
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-01-30 11:39:07 -0600

    working on authentication

--- api/server.js
+++ api/server.js
@@ -5,13 +5,37 @@ const dbConfig = require('../knexfile');
 const db = knex(dbConfig.development); 
 const server = express();
 //-----authentication------
+
 const bcrypt = require('bcryptjs'); 
-
 const credentials = req.body;
-
 const hash = bcrypt.hashSync(credentials.password, 14);
-
 credentials.password = hash;
+
+// find the user in the database by it's username then
+if (!user || !bcrypt.compareSync(credentials.password, user.password)) {
+  return res.status(401).json({ error: 'Incorrect credentials' });
+}
+// the user is valid, continue on
+
+server.post('/notesuser/login', (req, res) => {
+    let {username, password} = req.body; 
+
+    Users.findBy({username})
+    .first()
+    .then(user => {
+        //check that passwords match
+        if(user && bcrypt.compareSync(password, user.password)){
+            res.status(200).json({message: `Welcome ${user.username}!`});
+        } else {
+            // we will return 401 if the password or username are invalid
+            // we don't want to let attackers know when they have a good username
+            res.status(401).json({message: 'Invalid Credentials'})
+        }
+    })
+    .catch(error => {
+        res.status(500).json(error); 
+    })
+})
 
 //-------------------------
 
