dfdfd956e81cbc99a392f7a9a15661e41cc9c635
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-02-03 10:01:56 -0600

    working through suthentication

--- api/auth.js
+++ api/auth.js
@@ -1,4 +1,3 @@
-const axios = require('axios');
 const bcrypt = require('bcryptjs'); 
 const db = require('../knexfile')
 const jwt = require('jsonwebtoken');
@@ -17,6 +16,36 @@ function generateToken(user){
         expiresIn: '1h',
         jwtid: '12345',
     };
-    return jwt.sign(payload, options)
+    return jwt.sign(payload, options); 
+}
+
+function register(req, res){
+    let creds = req.body; 
+    const hash = bcrypt.hashSync(creds.password, 10);
+    creds.password = hash;
+
+    db('users')
+        .insert(creds)
+        .then(response => {
+            if(response) return res.this.status(200).send(response);
+        })
+        .catch(err => res.status(500).send(err));
+}
+
+function login(req, res){
+    let creds = req.body;
+
+    db('users')
+        .where({username: creds.username})
+        .first()
+        .then(user => {
+            if(user && bcrypt.compareSync(creds.password, user.password)){
+                const token = generateToken(user); 
+                res.status(200).json({token})                
+            }else{
+                res.status(401).json({message: "No User Found"});
+            }
+        })
+        .catch(err => res.status(500).send(err)); 
 }
 
